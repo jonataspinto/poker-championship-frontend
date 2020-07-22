c5e2e50e32fb4e7db507def40bf5934b3028a0f1
jonataspinto <jonatas.pferreira@b2wdigital.com>
2020-07-22 17:28:12 -0300

    chore: finish layout login route

--- src/containers/Login/Login.jsx
+++ src/containers/Login/Login.jsx
@@ -1,56 +1,98 @@
-import React from "react";
-// import PropTypes from "prop-types";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import {
   makeStyles,
   Grid,
   TextField,
   Button,
+  InputAdornment,
+  IconButton,
+  Avatar,
+  Typography,
 } from "@material-ui/core";
+import {
+  Visibility,
+  VisibilityOff,
+} from "@material-ui/icons";
+import { Google } from "styled-icons/boxicons-logos";
+import { UserTie } from "styled-icons/fa-solid";
 
-const useStyles = makeStyles(() => ({
-  root: {
-    "& > *": {
-      maxWidth: "55ch",
-      width: "100%",
-      desplay: "inline-flex",
-      justifyContent: "center",
-      alignItems: "center",
-    },
+import { LoginGoogle } from "../../config/firebase/auth";
+
+const useStyles = makeStyles((theme) => ({
+  avatar: {
+    margin: theme.spacing(1),
+    padding: theme.spacing(1),
+    width: "60px",
+    height: "60px",
   },
-  container: {
-    height: "100%",
-    paddingTop: "100px",
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  google: {
+    margin: theme.spacing(0, 0, 2),
   },
 }));
 
 const Login = () => {
   const classes = useStyles();
 
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
     <>
-      <Grid
-        container
-        direction="column"
-        justify="center"
-        alignItems="center"
-
-        className={classes.container}
-      >
-        <form noValidate autoComplete="off" style={{ height: "100%" }}>
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <UserTie style={{ width: "100%" }} />
+        </Avatar>
+        <Typography component="h3" variant="h5">
+          Faça login na sua conta
+        </Typography>
+        <form className={classes.form} noValidate>
           <TextField
-            id="email"
-            label="E-mail"
-            variant="outlined"
-            fullWidth
-            type="email"
-          />
-          <TextField
-            id="password"
-            label="Senha"
             variant="outlined"
             margin="normal"
+            required
             fullWidth
-            type="password"
+            id="email"
+            label="E-mail"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            label="Senha"
+            name="password"
+            type={showPassword ? "text" : "password"}
+            id="password"
+            autoComplete="current-password"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={() => setShowPassword(!showPassword)}
+                    onMouseDown={() => setShowPassword(!showPassword)}
+                  >
+                    {showPassword ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
           />
           <Button
             variant="contained"
@@ -58,26 +100,38 @@ const Login = () => {
             color="primary"
             margin="normal"
             fullWidth
+            className={classes.submit}
           >
-            Fazer Login
+            fazer login
           </Button>
           <Button
             variant="contained"
             size="medium"
-            fullWidth
+            color="secondary"
             margin="normal"
+            fullWidth
+            onClick={() => LoginGoogle()}
+            className={classes.google}
           >
-            Criar conta
+            <Google style={{ width: "24px", marginRight: "10px" }} />
+            login com google
           </Button>
-
+          <Grid container>
+            <Grid item xs>
+              <Link to="/">
+                Esqueceu sua conta?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link to="/">
+                Criar conta
+              </Link>
+            </Grid>
+          </Grid>
         </form>
-      </Grid>
+      </div>
     </>
   );
-};
-
-Login.propTypes = {
-
 };
 
 export default Login;
