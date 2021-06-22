8d801e432bb7f24aac1cb38ec08c5500be70ed1d
joaoprferreira <joaof082@gmail.com>
2021-06-21 23:40:17 -0300

    feat: implements typescript

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,10 @@ import { PersistGate } from "redux-persist/integration/react";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyles } from "./styles/themes/global";
 import { store, persistor } from "./store";
-import {Routes} from "./containers/routes";
+import { Routes } from "./containers/routes";
 import light from "./styles/themes/light";
 
 export default function App() {
-
   return (
     <Provider store={store || {}}>
       <PersistGate loading={null} persistor={persistor}>
