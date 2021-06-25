8c2406b09dcabbe17630447e97517e13ccb249f9
Jonatas Pinto <jonatastdb_jp@hotmail.com>
2021-06-25 18:46:51 -0300

    chore(JourneyList): draw createdAt value.

--- src/containers/Journey/Sections/JourneyList.tsx
+++ src/containers/Journey/Sections/JourneyList.tsx
@@ -13,6 +13,7 @@ import { ExpandMore, Block } from "@material-ui/icons";
 import { BoxPodiumPlayer } from "./BoxPodiumPlayer";
 import { CloseOrUpdateJourney } from "./CloseOrUpdateJourney";
 import { IJourney, IPlayer } from "../../../shared/interfaces"
+import { formatDate } from "../../../utils";
 
 interface JourneyListProps {
   journeys: IJourney[],
@@ -22,120 +23,122 @@ interface JourneyListProps {
 export const JourneyList = ({
   journeys = [],
   players = [],
-}: JourneyListProps) => (
-  <>
-    {journeys && journeys.length > 0 && journeys.map((journey) => {
-      const closedBy = players.find((player) => (
-        player.uuid === journey.closedBy
-      ));
+}: JourneyListProps) => {
+  return (
+    <>
+      {journeys && journeys.length > 0 && journeys.map((journey) => {
+        const closedBy = players.find((player) => (
+          player.uuid === journey.closedBy
+        ));
 
-      const podiums = Object.entries({
-        primeiro: players.find((player) => (
-          player.uuid === journey.podium.first
-        )),
-        segundo: players.find((player) => (
-          player.uuid === journey.podium.second
-        )),
-        terceiro: players.find((player) => (
-          player.uuid === journey.podium.third
-        )),
-        quarto: players.find((player) => (
-          player.uuid === journey.podium.fourth
-        )),
-        quinto: players.find((player) => (
-          player.uuid === journey.podium.fifth
-        )),
-      }).map((position) => ({
-        label: position[0],
-        player: { ...position[1] },
-      }));
+        const podiums = Object.entries({
+          primeiro: players.find((player) => (
+            player.uuid === journey.podium.first
+          )),
+          segundo: players.find((player) => (
+            player.uuid === journey.podium.second
+          )),
+          terceiro: players.find((player) => (
+            player.uuid === journey.podium.third
+          )),
+          quarto: players.find((player) => (
+            player.uuid === journey.podium.fourth
+          )),
+          quinto: players.find((player) => (
+            player.uuid === journey.podium.fifth
+          )),
+        }).map((position) => ({
+          label: position[0],
+          player: { ...position[1] },
+        }));
 
-      const otherScorers = Object.entries({
-        "melhor mão": players.find((player) => (
-          player.uuid === journey.bestHand
-        )),
-        "maior eliminador": players.find((player) => (
-          player.uuid === journey.biggestEliminator
-        )),
-      }).map((position) => ({
-        label: position[0],
-        player: { ...position[1]},
-      }));
+        const otherScorers = Object.entries({
+          "melhor mão": players.find((player) => (
+            player.uuid === journey.bestHand
+          )),
+          "maior eliminador": players.find((player) => (
+            player.uuid === journey.biggestEliminator
+          )),
+        }).map((position) => ({
+          label: position[0],
+          player: { ...position[1]},
+        }));
 
-      return (
-        <Accordion key={journey.id}>
-          <AccordionSummary
-            expandIcon={<ExpandMore />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-          >
-            <Typography
-              variant="subtitle1"
+        return (
+          <Accordion key={journey.id}>
+            <AccordionSummary
+              expandIcon={<ExpandMore />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
             >
-              {`Rodada #${journey.tag}`}
-            </Typography>
-            {journey.hasClosed && (
-              <Chip
-                variant="outlined"
-                size="small"
-                icon={<Block fontSize="inherit" color="error" />}
-                label="Fechada"
-                color="secondary"
-                style={{ marginLeft: "4px" }}
-              />
-            )}
-          </AccordionSummary>
-          <AccordionDetails
-            style={{
-              display: "flex",
-              flexDirection: "column",
-            }}
-          >
-            <Box
-              display="flex"
-              flexDirection="column"
-              gridGap={16}
-              width="100%"
-            >{
-              journey.hasClosed ? (
-              <>
+              <Typography
+                variant="subtitle1"
+              >
+                {`Rodada #${journey.tag} - ${formatDate(new Date(journey.createdAt), "dd/MM/yy")} `}
+              </Typography>
+              {journey.hasClosed && (
                 <Chip
                   variant="outlined"
                   size="small"
-                  label={`Encerrada por: ${closedBy?.name}`}
-                  avatar={(
-                    <Avatar
-                      src={closedBy?.photoURL}
-                    />
-                  )}
+                  icon={<Block fontSize="inherit" color="error" />}
+                  label="Fechada"
+                  color="secondary"
                   style={{ marginLeft: "4px" }}
                 />
-                <Divider />
-                {podiums.map((podium) => (
-                  <BoxPodiumPlayer
-                    key={podium.label}
-                    player={podium.player as IPlayer }
-                    label={`${podium.label} lugar: `}
-                  />
-                ))}
-                <Divider />
-                {otherScorers.map((podium) => (
-                  <BoxPodiumPlayer
-                    key={podium.label}
-                    player={podium.player as IPlayer }
-                    label={podium.label}
-                  />
-                ))}
-              </> ) : (
-                <CloseOrUpdateJourney
-                  players={players}
-                  journey={journey}
-                />
               )}
-            </Box>
-          </AccordionDetails>
-        </Accordion>
-      );
-    })}
-  </>
-);
+            </AccordionSummary>
+            <AccordionDetails
+              style={{
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <Box
+                display="flex"
+                flexDirection="column"
+                gridGap={16}
+                width="100%"
+              >{
+                journey.hasClosed ? (
+                <>
+                  {podiums.map((podium) => (
+                    <BoxPodiumPlayer
+                      key={podium.label}
+                      player={podium.player as IPlayer }
+                      label={`${podium.label} lugar: `}
+                    />
+                  ))}
+                  <Divider />
+                  {otherScorers.map((podium) => (
+                    <BoxPodiumPlayer
+                      key={podium.label}
+                      player={podium.player as IPlayer }
+                      label={podium.label}
+                    />
+                  ))}
+                  <Divider />
+                  <Chip
+                    variant="outlined"
+                    size="small"
+                    label={`Encerrada por: ${closedBy?.name}`}
+                    avatar={(
+                      <Avatar
+                        src={closedBy?.photoURL}
+                      />
+                    )}
+                    style={{ marginLeft: "4px" }}
+                  />
+                </> ) : (
+                  <CloseOrUpdateJourney
+                    players={players}
+                    journey={journey}
+                  />
+                )}
+              </Box>
+            </AccordionDetails>
+          </Accordion>
+        );
+      })}
+    </>
+  )
+};
