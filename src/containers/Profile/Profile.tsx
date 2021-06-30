e60f164e3d8f2da204f014a29c33f5af038a82ac
João Pedro <joaof082@gmail.com>
2021-06-30 14:05:45 -0300

    WIP

--- src/containers/Profile/Profile.tsx
+++ src/containers/Profile/Profile.tsx
@@ -3,6 +3,7 @@ import * as S from "./ProfileStyled";
 import { useSelector } from "react-redux";
 import { Helmet } from "react-helmet";
 import { RootState } from "../../store";
+import { IPlayer } from "../../shared/interfaces";
 export const Profile = () => {
   const {
     user,
@@ -15,12 +16,16 @@ export const Profile = () => {
     photoURL,
    } = user;
 
+  const handleEditProfile = (profile: IPlayer) => {
+    console.log(profile)
+  } 
+
   return (
     <S.ProfileContainer>
       <Helmet>
         <title>{`Poker | ${user?.name}`}</title>
       </Helmet>
-      <S.ProfileImage src={photoURL} alt="Imagem do usuário" />
+      <S.ProfileImage onClick={() => handleEditProfile(user)} src={photoURL} alt="Imagem do usuário" />
       <S.ProfileName>{name}</S.ProfileName>
       <S.Row>
         <S.EmailOutlined />
