148cd8399eba1879ba48390ecd2ee7837e36fde5
Jonatas Pinto <jonatastdb_jp@hotmail.com>
2021-07-15 23:07:41 -0300

    fix: remove space

--- src/utils/formatters/formatString.spec.ts
+++ src/utils/formatters/formatString.spec.ts
@@ -1,6 +1,5 @@
 import { formatStringToCapitalize } from "./formatString"
 
-
 describe("Format string to Capitalize.", () => {
   test("should to format string poker to Poker", () => {
     const formatedString = formatStringToCapitalize("poker");
