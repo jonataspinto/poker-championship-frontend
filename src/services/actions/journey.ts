206565f07dedb3c98c550d23a6162ef4b520e633
jonataspinto <jonatastdb_jp@hotmail.com>
2025-05-12 20:39:21 -0300

    refactor: convert JourneyService methods to standalone async functions

--- src/services/actions/journey.ts
+++ src/services/actions/journey.ts
@@ -1,27 +1,27 @@
+"use server";
+
 import { HttpClient } from "../clients/httpClient";
 
 const client = new HttpClient<Journey, JourneyDTO>(
   process.env.NEXT_PUBLIC_BASE_URL || ""
 );
 
-export class JourneyService {
-  static async list() {
-    return client.get("/journeys", {
-      next: {
-        tags: ["list-journeys"]
-      }
-    }) as unknown as Promise<JourneyDTO[]>;
-  }
+export async function listJourneys() {
+  return client.get("/journeys", {
+    next: {
+      tags: ["list-journeys"]
+    }
+  }) as unknown as Promise<JourneyDTO[]>;
+}
 
-  static async getById(id: string) {
-    return client.get(`/journeys/${id}`);
-  }
+export async function getJourneyById(id: string) {
+  return client.get(`/journeys/${id}`);
+}
 
-  static async update(id: string, payload: Partial<Journey>) {
-    return client.put(`/journeys/${id}`, payload as Journey);
-  }
+export async function updateJourney(id: string, payload: Partial<Journey>) {
+  return client.put(`/journeys/${id}`, payload as Journey);
+}
 
-  static async close(id: string) {
-    return client.put(`/journeys/${id}/close`, {} as Journey);
-  }
+export async function closeJourney(id: string) {
+  return client.put(`/journeys/${id}/close`, {} as Journey);
 }
