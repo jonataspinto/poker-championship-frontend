21c0ee3040c823ea869673ccc61e66353b384f12
jonataspinto <jonatastdb_jp@hotmail.com>
2025-05-12 23:27:15 -0300

    feat: enhance Podium component layout with improved styling and alignment

--- src/app/rodadas/_components/Podium/index.tsx
+++ src/app/rodadas/_components/Podium/index.tsx
@@ -9,27 +9,27 @@ export async function Podium({
 }) {
   return (
     <div className="podium px-4 overflow-hidden">
-      <div className="first font-bold min-w-28">
+      <div className="first font-bold min-w-28 justify-center">
         <p className="truncate">
           {podium?.first && players.get(podium?.first)?.name}
         </p>
       </div>
 
-      <div className="second truncate mb-1">
+      <div className="second truncate mb-1 justify-center">
         <p className="truncate">
           {podium?.second && players.get(podium?.second)?.name}
         </p>
       </div>
 
-      <div className="third flex mb-1">
+      <div className="third flex mb-1 justify-center">
         <p className="truncate">
           {podium?.third && players.get(podium?.third)?.name}
         </p>
       </div>
 
-      <div className="first-base py-4 font-bold">1</div>
-      <div className="second-base py-4 font-bold">2</div>
-      <div className="third-base py-4 font-bold">3</div>
+      <div className="first-base py-4 font-bold text-center">1</div>
+      <div className="second-base py-4 font-bold text-center">2</div>
+      <div className="third-base py-4 font-bold text-center">3</div>
     </div>
   );
 }
