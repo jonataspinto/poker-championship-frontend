5ee23481971f8030ed99ace2a22bc8e369dba115
jonataspinto <jonatastdb_jp@hotmail.com>
2025-05-12 22:07:38 -0300

    feat: update Podium component layout and styles for better grid alignment

--- src/app/rodadas/_components/Podium/index.tsx
+++ src/app/rodadas/_components/Podium/index.tsx
@@ -8,23 +8,28 @@ export async function Podium({
   players: Map<string, PlayerDTO>;
 }) {
   return (
-    <div className="podium px-4">
-      <div className="first">
-        <span className="font-bold"> </span>
-        {podium?.first && players.get(podium?.first)?.name}
+    <div className="podium px-4 overflow-hidden">
+      <div className="first font-bold min-w-28">
+        <p className="truncate">
+          {podium?.first && players.get(podium?.first)?.name}
+        </p>
       </div>
 
-      <div className="second">
-        {podium?.second && players.get(podium?.second)?.name}
+      <div className="second truncate mb-1">
+        <p className="truncate">
+          {podium?.second && players.get(podium?.second)?.name}
+        </p>
       </div>
 
-      <div className="third">
-        {podium?.third && players.get(podium?.third)?.name}
+      <div className="third flex mb-1">
+        <p className="truncate">
+          {podium?.third && players.get(podium?.third)?.name}
+        </p>
       </div>
 
       <div className="first-base py-4 font-bold">1</div>
       <div className="second-base py-4 font-bold">2</div>
-      <div className="third-base py-4 font-bold">2</div>
+      <div className="third-base py-4 font-bold">3</div>
     </div>
   );
 }
