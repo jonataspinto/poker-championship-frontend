1892cc4d6a32f41eb371a99c29c6c058dba2f32f
jonataspinto <jonatastdb_jp@hotmail.com>
2025-05-13 18:34:33 -0300

    fix: add CREATE action to JOURNEY_ACTIONS_EVENT_KEY enum for completeness

--- src/app/rodadas/_components/journeyActionsEventManager.ts
+++ src/app/rodadas/_components/journeyActionsEventManager.ts
@@ -1,7 +1,8 @@
 import { EventManager } from "@/utils/EventManager";
 
 export enum JOURNEY_ACTIONS_EVENT_KEY {
-  UPDATE = "JOURNEY_UPDATE"
+  UPDATE = "JOURNEY_UPDATE",
+  CREATE = "JOURNEY_CREATE"
 }
 
 export const journeyActionsEventManager = new EventManager();
