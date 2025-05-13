b4db610be2734381d64efd27628940b351eacc67
jonataspinto <jonatastdb_jp@hotmail.com>
2025-05-12 22:07:53 -0300

    feat: enhance SubmitButton to accept props for custom class names

--- src/app/rodadas/_components/JourneyActions/SubmitButton.tsx
+++ src/app/rodadas/_components/JourneyActions/SubmitButton.tsx
@@ -2,12 +2,18 @@
 
 import { useFormStatus } from "react-dom";
 import { Spinner } from "@/components";
+import { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
 
-export function SubmitButton() {
+export function SubmitButton(props: ComponentProps<"button">) {
   const { pending } = useFormStatus();
 
   return (
-    <button disabled={pending} type="submit" className="btn-primary">
+    <button
+      disabled={pending}
+      type="submit"
+      className={twMerge("btn-primary", props.className)}
+    >
       {pending ? <Spinner /> : "Salvar"}
     </button>
   );
