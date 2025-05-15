e303a3661fe4ea1ef7e32282221cbccdd38a7c92
jonataspinto <jonatastdb_jp@hotmail.com>
2025-05-15 18:19:12 -0300

    refactor: adjust Avatar container size in NewJourneyForm for better alignment

--- src/app/rodadas/_components/CreateNewJourney/NewJourneyForm.tsx
+++ src/app/rodadas/_components/CreateNewJourney/NewJourneyForm.tsx
@@ -69,7 +69,7 @@ export function NewJourneyForm({
               htmlFor={player.id}
               className="inline-flex gap-4 w-full p-4 text-gray-500 bg-white border-2 border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 peer-checked:border-blue-600 dark:peer-checked:border-blue-600 hover:text-gray-600 dark:peer-checked:text-gray-300 peer-checked:text-gray-600 hover:bg-gray-50 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700"
             >
-              <div className="relative rounded-full overflow-hidden bg-gray-200 min-w-[80px] h-[80px] flex items-center justify-center">
+              <div className="relative rounded-full overflow-hidden bg-gray-200 min-w-[70px] h-[70px] flex items-center justify-center">
                 <Avatar
                   fill
                   src={player.photoURL || ""}
