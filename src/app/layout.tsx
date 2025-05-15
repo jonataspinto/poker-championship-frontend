4325dcd777fb981d64d5fa08323bc021d8342fb5
jonataspinto <jonatastdb_jp@hotmail.com>
2025-05-15 14:30:46 -0300

    feat: add ToastContainer to RootLayout for improved user notifications

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Header } from "@/components/Header";
+
+import { Header, ToastContainer } from "@/components";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
         {children}
         <div id="modal" />
         <div id="menu" />
+        <ToastContainer />
       </body>
     </html>
   );
