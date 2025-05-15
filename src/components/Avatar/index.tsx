7f27bbe93258f972cf79d3599727ecd5604e180a
jonataspinto <jonatastdb_jp@hotmail.com>
2025-05-15 18:18:01 -0300

    fix: handle optional title prop in Avatar component and ensure client usage

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,12 +1,15 @@
+"use client";
+
 import Image, { ImageProps } from "next/image";
 
-export function Avatar(props: ImageProps) {
+export function Avatar({ ...props }: ImageProps) {
   return (
     <Image
       onError={(event: React.SyntheticEvent<HTMLImageElement, Event>) => {
         (event.target as HTMLImageElement).src =
-          `/api/user-image?name=${props.title}`;
+          `/api/user-image?name=${props?.title}`;
       }}
+      fill
       {...props}
       alt={props.alt || "User avatar"}
     />
