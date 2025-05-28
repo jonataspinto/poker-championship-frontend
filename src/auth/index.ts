512a22a391b50c6a1cfb24333d778591f4f43c1f
jonataspinto <jonatastdb_jp@hotmail.com>
2025-05-27 22:56:05 -0300

    feat: enhance Google authentication flow with Firebase integration

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,8 +1,20 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { signInWithCredential, GoogleAuthProvider, User } from "firebase/auth";
 
 import { getPlayerById } from "@/services/actions";
 import { PlayerMapper } from "@/utils/mappers";
+import { firebaseAuth } from "@/services/clients/firebaseClient";
+
+function extractTokensFromStsTokenManager(user: User) {
+  // @ts-expect-error this key exists in the Firebase user object
+  const { accessToken, refreshToken } = user.stsTokenManager;
+
+  return {
+    accessToken,
+    refreshToken
+  };
+}
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
@@ -12,23 +24,53 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     })
   ],
   callbacks: {
-    async signIn({ account, profile, credentials, user, email }) {
-      console.log("🚀 ~ signIn:", {
-        account,
-        profile,
-        credentials,
-        user,
-        email
-      });
+    async signIn({ account }) {
+      if (account?.provider === "google" && account?.id_token) {
+        try {
+          // Criar uma credencial do Firebase com o token do Google
+          const credential = GoogleAuthProvider.credential(account.id_token);
 
-      return true;
+          // Fazer login no Firebase
+          const firebaseUser = await signInWithCredential(
+            firebaseAuth,
+            credential
+          );
+
+          const { accessToken, refreshToken } =
+            extractTokensFromStsTokenManager(firebaseUser.user);
+
+          // @ts-expect-error is possible to set custom properties on the account object
+          account.accessToken = accessToken;
+
+          // @ts-expect-error is possible to set custom properties on the account object
+          account.refresh_token = refreshToken;
+
+          // TODO: Verificar se o usuário existe em /players e criar se não existir
+          return true;
+        } catch (error) {
+          console.error("Erro ao autenticar no Firebase:", error);
+          return false;
+        }
+      }
+
+      return false;
+    },
+    async jwt({ account, token }) {
+      if (account?.accessToken) {
+        token.accessToken = account.accessToken;
+        token.refreshToken = account.refresh_token;
+      }
+
+      return token;
     },
     async session({ session, token }) {
+      const { accessToken, refreshToken } = token;
+
       const profile = await getPlayerById(token.email as string);
 
       const user = PlayerMapper.toDomain({ ...session.user, ...profile });
 
-      return { ...session, token, user };
+      return { ...session, accessToken, refreshToken, user };
     }
   }
 });
