849d78d80eb6fd34702a1a6d859fc75bfc566e57
jonataspinto <jonatastdb_jp@hotmail.com>
2025-06-03 15:21:43 -0300

    feat: update Home component to integrate Ranking and MyResults with skeleton loading states

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Suspense } from "react";
-import { Ranking } from "@/components/_modules/Ranking";
+import { Ranking, RankingSkeleton } from "@/components/_modules/Ranking";
+import { MyResults, MyResultsSkeleton } from "@/components/_modules/MyResults";
 
 export const metadata = {
   title: "Poker Championship",
@@ -8,38 +9,14 @@ export const metadata = {
 
 export default async function Home() {
   return (
-    <div className="container mx-auto px-4 py-8 grid md:grid-cols-2 gap-8">
-      <div className="flex flex-col gap-8 h-full">
-        <Suspense fallback={<div>Loading...</div>}>
-          <section className="bg-gray-800 p-8 rounded h-full">
-            <h1 className="text-3xl font-bold mb-4">Meus resultados</h1>
-            {/* Example my data */}
-            <div className="rounded">
-              <p className="text-lg">Total Wins: 5</p>
-              <p className="text-lg">Total Rounds Played: 10</p>
-              <p className="text-lg">Best Hand: Royal Flush</p>
-            </div>
-          </section>
-        </Suspense>
-        <Suspense fallback={<div>Loading...</div>}>
-          <section className="bg-gray-800 p-8 rounded h-full">
-            <h1 className="text-3xl font-bold mb-4">Última rodada</h1>
-            {/* Example details for the last journey */}
-            <div className="rounded">
-              <p className="text-lg font-semibold">Date: 2024-06-10</p>
-              <p className="text-lg">Winner: John Doe</p>
-              <p className="text-lg">Buy-ins: 10</p>
-              <p className="text-lg">Total Pot: $500</p>
-            </div>
-          </section>
-        </Suspense>
-      </div>
+    <div className="container mx-auto px-4 py-8">
+      <Suspense fallback={<RankingSkeleton />}>
+        <Ranking />
+      </Suspense>
 
-      <section className="relative overflow-x-auto">
-        <Suspense fallback={<div>Loading...</div>}>
-          <Ranking />
-        </Suspense>
-      </section>
+      <Suspense fallback={<MyResultsSkeleton />}>
+        <MyResults />
+      </Suspense>
     </div>
   );
 }
