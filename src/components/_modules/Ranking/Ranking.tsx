77ff4cf1b52b829eafca8f5e6a3df3d76eca58bd
jonataspinto <jonatastdb_jp@hotmail.com>
2025-06-03 16:23:38 -0300

    feat: enhance Ranking component with className prop and loading simulation

--- src/components/_modules/Ranking/Ranking.tsx
+++ src/components/_modules/Ranking/Ranking.tsx
@@ -1,11 +1,17 @@
+import { twMerge } from "tailwind-merge";
+import { ComponentProps } from "react";
 import { Avatar } from "@/components";
 import { listPlayers } from "@/services/actions";
 
-export async function Ranking() {
+export async function Ranking({
+  className,
+  ...rest
+}: ComponentProps<"section">) {
   const players = await listPlayers();
+  await new Promise((resolve) => setTimeout(resolve, 1000)); // Simulate loading delay
 
   return (
-    <section className="flex flex-col gap-4">
+    <section className={twMerge("flex flex-col gap-4", className)} {...rest}>
       <h1 className="text-2xl font-bold">Ranking</h1>
       {players.map((player) => (
         <div key={player?.id} className="flex gap-4 items-center">
@@ -14,9 +20,9 @@ export async function Ranking() {
             height={56}
             alt={player?.name || ""}
             src={player?.photoURL || ""}
-            className="rounded-full bg-gray-700"
+            className="rounded-full bg-gray-700 animate-fadeId"
           />
-          <div>
+          <div className="animate-fadeId">
             <p className="text-base font-semibold text-white capitalize font-sans">
               {player?.name}
             </p>
