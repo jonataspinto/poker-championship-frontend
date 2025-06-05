bbed7804bb277ee8e10e501a337f41068e36ff76
jonataspinto <jonatastdb_jp@hotmail.com>
2025-06-05 00:19:52 -0300

    feat: enhance Header component with sticky positioning and background color

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import { DrawerMenu } from "./DrawerMenu";
 
 export function Header() {
   return (
-    <header className="flex items-center justify-between px-4 shadow-sm">
+    <header className="flex items-center justify-between px-4 shadow-sm sticky top-0 z-10 bg-background">
       <DrawerMenu />
       <BrandLogo model="minimalist" />
     </header>
