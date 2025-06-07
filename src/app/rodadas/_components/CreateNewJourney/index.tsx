b300f7c816356f62a74c9d14a62c0fa266a5a1f6
jonataspinto <jonatastdb_jp@hotmail.com>
2025-06-06 22:29:05 -0300

    feat: refactor CreateNewJourney and Page components for improved structure and async data handling

--- src/app/rodadas/_components/CreateNewJourney/index.tsx
+++ src/app/rodadas/_components/CreateNewJourney/index.tsx
@@ -1,16 +1,21 @@
-import { listPlayers } from "@/services/actions";
-import { listSeasons } from "@/services/actions/seasons";
 import { NewJourneyDialog } from "./NewJourneyDialog";
 import { NewJourneyForm } from "./NewJourneyForm";
 
-export async function CreateNewJourney({ disabled }: { disabled?: boolean }) {
+export async function CreateNewJourney() {
+  const { listJourneys, listPlayers, listSeasons } = await import(
+    "@/services/actions/"
+  );
+
+  const journeys = await listJourneys();
   const players = await listPlayers();
   const seasons = await listSeasons();
 
   const seasonId = seasons[0]?.id;
 
   return (
-    <NewJourneyDialog disabled={disabled}>
+    <NewJourneyDialog
+      disabled={journeys?.some((journey) => !journey?.hasClosed)}
+    >
       <NewJourneyForm players={players} seasonId={seasonId} />
     </NewJourneyDialog>
   );
--- src/app/rodadas/page.tsx
+++ src/app/rodadas/page.tsx
@@ -1,4 +1,4 @@
-import { listJourneys } from "@/services/actions";
+import { Suspense } from "react";
 import { DetailPanel, PageContainer } from "@/components";
 import { Divider } from "@/components/Divider";
 import { Podium } from "./_components/Podium";
@@ -6,38 +6,50 @@ import { BestHandAndEliminator } from "./_components/BestHandAndEliminator";
 import { JourneyActions } from "./_components/JourneyActions";
 import { CreateNewJourney } from "./_components/CreateNewJourney";
 
-export default async function Page() {
-  const journeys = await listJourneys();
+export default function Page() {
+  return (
+    <PageContainer className="flex flex-col items-center gap-4 w-full">
+      <CreateNewJourney />
+      <Suspense
+        fallback={
+          <div className="w-full h-96 flex items-center justify-center">
+            Carregando...
+          </div>
+        }
+      >
+        <JourneyList />
+      </Suspense>
+    </PageContainer>
+  );
+}
 
+export async function JourneyList() {
   const title = (_journey: JourneyDTO) =>
     `Rodada #${_journey?.tag} - ${new Intl.DateTimeFormat("pt-BR", {
       dateStyle: "long"
     }).format(new Date(_journey?.createdAt))}`;
 
-  return (
-    <PageContainer className="flex flex-col items-center gap-4 w-full">
-      <CreateNewJourney
-        disabled={journeys?.some((journey) => !journey?.hasClosed)}
-      />
-      {journeys?.map((journey, index) => (
-        <DetailPanel.Root
-          key={journey.id}
-          open={index === 0 && !journey?.hasClosed}
-          className="border border-solid border-gray-800 rounded-lg w-full"
-        >
-          <DetailPanel.Summary>
-            <span className="text-lg font-bold px-4">{title(journey)}</span>
-          </DetailPanel.Summary>
-          <DetailPanel.Content className="flex flex-col gap-4 mt-0 pb-4">
-            <Divider className="bg-gray-800" />
-            <Podium podium={journey.podium} />
-            <Divider className="bg-gray-800" />
-            <BestHandAndEliminator journey={journey} />
-            <Divider className="bg-gray-800" />
-            <JourneyActions journey={journey} />
-          </DetailPanel.Content>
-        </DetailPanel.Root>
-      ))}
-    </PageContainer>
-  );
+  const { listJourneys } = await import("@/services/actions");
+
+  const journeys = await listJourneys();
+
+  return journeys?.map((journey, index) => (
+    <DetailPanel.Root
+      key={journey.id}
+      open={index === 0 && !journey?.hasClosed}
+      className="border border-solid border-gray-800 rounded-lg w-full"
+    >
+      <DetailPanel.Summary>
+        <span className="text-lg font-bold px-4">{title(journey)}</span>
+      </DetailPanel.Summary>
+      <DetailPanel.Content className="flex flex-col gap-4 mt-0 pb-4">
+        <Divider className="bg-gray-800" />
+        <Podium podium={journey.podium} />
+        <Divider className="bg-gray-800" />
+        <BestHandAndEliminator journey={journey} />
+        <Divider className="bg-gray-800" />
+        <JourneyActions journey={journey} />
+      </DetailPanel.Content>
+    </DetailPanel.Root>
+  ));
 }
