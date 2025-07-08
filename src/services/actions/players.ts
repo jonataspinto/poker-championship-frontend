e2b80e87d2b61597ca79ffae3964497da5f56629
jonataspinto <jonatastdb_jp@hotmail.com>
2025-07-08 20:28:24 -0300

    refactor: streamline player service initialization by moving repository and controller instantiation outside of functions

--- src/services/actions/players.ts
+++ src/services/actions/players.ts
@@ -4,25 +4,19 @@ import { PlayersRepository } from "@/server/repositories/PlayersRepository";
 import { PlayerController } from "@/server/controllers/PlayerController";
 import { FirestoreAdapterDB } from "../database";
 
+const playersRepository = new PlayersRepository(
+  new FirestoreAdapterDB("users")
+);
+
+const playerController = new PlayerController(playersRepository);
+
 export async function listPlayers(): Promise<PlayerDTO[]> {
-  const playersRepository = new PlayersRepository(
-    new FirestoreAdapterDB("users")
-  );
-
-  const playerController = new PlayerController(playersRepository);
-
   const players = await playerController.index();
 
   return players;
 }
 
 export async function getPlayerById(id: string) {
-  const playersRepository = new PlayersRepository(
-    new FirestoreAdapterDB("users")
-  );
-
-  const playerController = new PlayerController(playersRepository);
-
   const player = await playerController.show(id);
 
   if (!player) {
