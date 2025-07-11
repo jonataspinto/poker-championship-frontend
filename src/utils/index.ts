2d2c58e3965e2a5f7527f2b4be438d396d6a941b
jonataspinto <jonatastdb_jp@hotmail.com>
2025-07-11 15:09:50 -0300

    refactor: reorganize exports in index.ts for better structure

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,6 @@
+export * from "./constants";
 export * from "./EventManager";
 export * from "./mappers";
 export * from "./podiumPosition";
+export * from "./orderPlayersRanking";
+export * from "./sanitizeObject";
