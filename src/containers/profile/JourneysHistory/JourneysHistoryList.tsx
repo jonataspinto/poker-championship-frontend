2649c085bf8d3ea9782206728b7b96396994f98f
jonataspinto <jonatastdb_jp@hotmail.com>
2025-07-13 19:09:27 -0300

    refactor: simplify journey fetching logic in JourneysHistoryList function

--- src/containers/profile/JourneysHistory/JourneysHistoryList.tsx
+++ src/containers/profile/JourneysHistory/JourneysHistoryList.tsx
@@ -4,17 +4,15 @@ import { LastJourneyCard } from "./LastJourneyCard";
 export async function JourneysHistoryList() {
   const session = await auth();
   const playerId = session?.user?.id;
-  let journeys: JourneyDTO[] = [];
+  const searchParams = new URLSearchParams();
 
-  try {
-    if (playerId) {
-      const searchParams = new URLSearchParams({ playerId });
-      const { listJourneys } = await import("@/services/actions");
-      journeys = await listJourneys(searchParams);
-    }
-  } catch (error) {
-    console.error("Error fetching journeys:", error);
+  if (playerId) {
+    searchParams.set("playerId", playerId);
   }
+
+  const { listJourneys } = await import("@/services/actions");
+
+  const journeys = await listJourneys(searchParams);
 
   if (!journeys || journeys.length === 0) {
     return <p className="text-gray-500">Nenhuma rodada encontrada.</p>;
@@ -22,21 +20,17 @@ export async function JourneysHistoryList() {
 
   const { podiumPosition } = await import("@/utils");
 
-  return (
-    <>
-      {journeys.map((journey) => (
-        <LastJourneyCard.Container key={journey?.id}>
-          <LastJourneyCard.IconWrapper>
-            <LastJourneyCard.Icon alt="Trophy icon" src="/icons/trophy.svg" />
-          </LastJourneyCard.IconWrapper>
-          <LastJourneyCard.Details>
-            <LastJourneyCard.Title>Rodada {journey?.tag}</LastJourneyCard.Title>
-            <LastJourneyCard.Description>
-              {podiumPosition(journey?.podium, playerId)}
-            </LastJourneyCard.Description>
-          </LastJourneyCard.Details>
-        </LastJourneyCard.Container>
-      ))}
-    </>
-  );
+  return journeys.map((journey) => (
+    <LastJourneyCard.Container key={journey?.id}>
+      <LastJourneyCard.IconWrapper>
+        <LastJourneyCard.Icon alt="Trophy icon" src="/icons/trophy.svg" />
+      </LastJourneyCard.IconWrapper>
+      <LastJourneyCard.Details>
+        <LastJourneyCard.Title>Rodada {journey?.tag}</LastJourneyCard.Title>
+        <LastJourneyCard.Description>
+          {podiumPosition(journey?.podium, playerId)}
+        </LastJourneyCard.Description>
+      </LastJourneyCard.Details>
+    </LastJourneyCard.Container>
+  ));
 }
