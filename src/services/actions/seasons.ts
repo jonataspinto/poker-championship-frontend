8c04e44b59f68ee10a5dbefd1054e64310e4c351
jonataspinto <jonatastdb_jp@hotmail.com>
2025-07-22 22:35:54 -0300

    feat: add rounds management to Season and implement createNewRound function

--- src/services/actions/seasons.ts
+++ src/services/actions/seasons.ts
@@ -38,6 +38,7 @@ export async function createSeason(data: Pick<Season, "title">) {
       ...data,
       hasClosed: false,
       journeys: [],
+      rounds: [],
       tag: 0
     });
 
@@ -50,4 +51,38 @@ export async function createSeason(data: Pick<Season, "title">) {
 
 export async function closeSeason(closedBy: string) {
   return seasonController.closeSeason(closedBy);
+}
+
+export async function updateSeason(id: string, data: Partial<Season>) {
+  return seasonController.update(id, data as Season);
+}
+
+export async function createNewRound(
+  seasonId: string,
+  data: Pick<Round, "players">
+) {
+  if (!seasonId) {
+    throw new Error("Season ID is required to create a new round.");
+  }
+
+  const season = await seasonController.show(seasonId);
+
+  if (!season) {
+    throw new Error(`Season with ID ${seasonId} not found.`);
+  }
+
+  const currentTag = season?.rounds?.length ?? 0;
+
+  const newRound = {
+    ...data,
+    tag: currentTag + 1,
+    hasClosed: false,
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+    id: crypto.randomUUID()
+  };
+
+  season.rounds!.push(newRound);
+
+  return seasonController.update(seasonId, season);
 }
--- types/Season.d.ts
+++ types/Season.d.ts
@@ -1,8 +1,30 @@
+type RoundPodium = Record<
+  "first" | "second" | "third" | "fourth" | "fifth",
+  PlayerDTO
+>;
+
+type Round = {
+  tag: number;
+  players: PlayerDTO[];
+  hasClosed: boolean;
+  biggestEliminator?: PlayerDTO;
+  bestHand?: PlayerDTO;
+  closedBy?: PlayerDTO;
+  podium?: RoundPodium;
+};
+
+type RoundDTO = Round & {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
 type Season = {
   tag: number;
   title: string;
   hasClosed: boolean;
   journeys: string[];
+  rounds?: RoundDTO[];
   closedBy?: string;
 };
 
