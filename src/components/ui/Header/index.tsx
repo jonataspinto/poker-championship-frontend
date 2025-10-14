ecdd74b96421b9c14853886be77ce67ef07102a3
jonataspinto <jonatastdb_jp@hotmail.com>
2025-10-14 18:53:26 -0300

    fix: add container class to header for proper alignment

--- src/components/ui/Header/index.tsx
+++ src/components/ui/Header/index.tsx
@@ -3,7 +3,7 @@ import { DrawerMenu } from "./DrawerMenu";
 
 export function Header() {
   return (
-    <header className="flex items-center justify-between px-4 shadow-sm sticky top-0 z-10 bg-background">
+    <header className="container mx-auto flex items-center justify-between px-4 shadow-sm sticky top-0 z-10 bg-background">
       <DrawerMenu />
       <BrandLogo model="minimalist" />
     </header>
